5c89f7dec868583eccc9616513ef6cf7da4c8a75
Navid Ihsas <navedahsas77@gmail.com>
2023-12-18 12:10:23 -0800

    Add project files.

---
+++ Dari.DateTime/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Dari.DateTime;
+
+public static class Extensions
+{
+    /// <summary>
+    ///     تبدیل تاریخ شمسی به میلادی
+    /// </summary>
+    /// <param name="date">تاریخ میلادی</param>
+    /// <param name="dateFormat">فرمت نمایش تاریخ شمسی (اختیاری)</param>
+    /// <returns></returns>
+    public static string ToDariDateTime(this System.DateTime date, DateFormat dateFormat = DateFormat.YearMonthDay)
+    {
+        var format = dateFormat.GetFormat();
+        return date.ToString(format, new CultureInfo("prs-AF"));
+    }
+
+    /// <summary>
+    ///     تبدیل تاریخ شمسی به میلادی با فرمت دلخواه
+    /// </summary>
+    /// <param name="date">تاریخ میلادی</param>
+    /// <param name="format">فرمت</param>
+    /// <returns></returns>
+    public static string ToDariDateTime(this System.DateTime date, string format)
+    {
+        return date.ToString(format, new CultureInfo("prs-AF"));
+    }
+}
---
+++ Dari.DateTime/Tools.cs
@@ -0,0 +1,30 @@
+﻿namespace Dari.DateTime;
+
+public static class Tools
+{
+    public static string GetFormat(this DateFormat dateFormat)
+    {
+        return dateFormat switch
+        {
+            DateFormat.YearMonthDay => "yyyy/MM/dd",
+            DateFormat.YearMonthNameDay => "yyyy/MMMM/dd",
+            DateFormat.YearDayMonthName => "yyyy/dd/MMMM",
+            _ => throw new ArgumentOutOfRangeException(nameof(dateFormat), dateFormat, null)
+        };
+    }
+}
+
+public enum DateFormat
+{
+    /// <summary>
+    ///     خروجی: "1402/09/27"
+    /// </summary>
+    YearMonthDay,
+
+    /// <summary>
+    ///     خروجی: 1402/قوس/27
+    /// </summary>
+    YearMonthNameDay,
+
+    YearDayMonthName
+}
